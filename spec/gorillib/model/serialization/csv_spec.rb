a9c77527c1e16ee2d1413d6f5c88c82573f823f1
Alex Vinnik <alex.vinnik@infochimps.com>
2014-02-12 18:23:25 -0600

    Changes based on code review
    Change-Id: Idda90d368d2a4293077f3935ef4789603f3df2c0

--- spec/gorillib/model/serialization/csv_spec.rb
+++ spec/gorillib/model/serialization/csv_spec.rb
@@ -8,12 +8,13 @@ require 'gorillib/builder'
 require 'gorillib/model/serialization/csv'
 
 describe Gorillib::Model::LoadFromCsv, :model_spec, :builder_spec do
-  
+
   context ".load_csv" do
 
-    let(:expected_engine) do {:name=>:Wankel, :carburetor=>:no, :volume=>1, 
-    :cylinders=>982, :owner=>"who_am_i"} end
-    
+    let(:expected_engine) do 
+      {:name=>:Wankel, :carburetor=>:no, :volume=>1, :cylinders=>982, :owner=>"who_am_i"}
+    end
+
     before :each do
       engine_class.class_eval { include Gorillib::Model::LoadFromCsv }
       csv_file = double('csv_file')
@@ -21,12 +22,12 @@ describe Gorillib::Model::LoadFromCsv, :model_spec, :builder_spec do
       csv_file.stub(:each).and_yield(expected_engine.values)
       CSV.should_receive(:open).and_yield(csv_file)
     end
-    
+
     it "loads from file" do
-      engine_class.load_csv('test').to_wire().first.keep_if{|k,| k != :_type}.should 
+      engine_class.load_csv('test').to_wire().first.keep_if{|k,| k != :_type}.should
         eql(expected_engine)
      end
-     
+
      it "loads from file with block" do
        expect { |b| engine_class.load_csv('test', &b) }.to yield_with_args(engine_class)
      end
